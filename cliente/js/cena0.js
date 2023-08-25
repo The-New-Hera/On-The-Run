5537da5fc3f15e55c7cd294ab0ed43fcf6502982
Natalia Ferrari <natalia.ef2006@aluno.ifsc.edu.br>
2023-08-25 18:32:48 +0000

    Teste de animação de personagem

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -5,10 +5,32 @@ export default class cena0 extends Phaser.Scene {
 
   preload () {
     this.load.image('ifsc-sj-2014', '../assets/ifsc-sj-2014.png')
+    this.load.spritesheet('Derek', '../assets/Derek.png',
+      {
+        frameWidth: 64,
+        frameHeight: 64
+      }
+    )
   }
 
   create () {
     this.add.image(400, 225, 'ifsc-sj-2014')
+    this.personagem = this.physics.add.sprite(400, 225, 'Derek')
+      .setInteractive()
+      .on('pointerdown', () => { 
+        this.personagem.anims.play('Derek-direita')
+        this.personagem.setVelocityX(100)
+      })
+
+    this.anims.create({
+      key: 'Derek-direita',
+      frames: this.anims.generateFrameNumbers('Derek', {
+        start: 8,
+        end: 11
+      }),
+      frameRate: 6,
+      repeat: -1
+    })
   }
 
   update () { }
