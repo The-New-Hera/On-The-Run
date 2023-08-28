42911cd9c2814eb0231e0e0fe4029eaff4e0ceb9
Natalia Ferrari <natalia.ef2006@aluno.ifsc.edu.br>
2023-08-28 20:20:29 +0000

    Animação a partir de botões

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -11,16 +11,39 @@ export default class cena0 extends Phaser.Scene {
         frameHeight: 64
       }
     )
+
+    this.load.spritesheet('direita', '../assets/direita.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+
+    this.load.spritesheet('esquerda', '../assets/esquerda.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+
+    this.load.spritesheet('cima', '../assets/cima.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
   }
 
   create () {
+    /* Imagem de fundo */
     this.add.image(400, 225, 'ifsc-sj-2014')
+
+    /* Personagem */
     this.personagem = this.physics.add.sprite(400, 225, 'Derek')
-      .setInteractive()
-      .on('pointerdown', () => { 
-        this.personagem.anims.play('Derek-direita')
-        this.personagem.setVelocityX(100)
-      })
+
+    /* Animações */
+    this.anims.create({
+      key: 'Derek-parado',
+      frames: this.anims.generateFrameNumbers('Derek', {
+        start: 0,
+        end: 0
+      }),
+      frameRate: 1
+    })
 
     this.anims.create({
       key: 'Derek-direita',
@@ -31,6 +54,66 @@ export default class cena0 extends Phaser.Scene {
       frameRate: 6,
       repeat: -1
     })
+
+    this.anims.create({
+      key: 'Derek-esquerda',
+      frames: this.anims.generateFrameNumbers('Derek', {
+        start: 4,
+        end: 7
+      }),
+      frameRate: 6,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: 'Derek-cima',
+      frames: this.anims.generateFrameNumbers('Derek', {
+        start: 0,
+        end: 2
+      }),
+      frameRate: 6,
+      repeat: -1
+    })
+
+    /* Botões */
+    this.direita = this.add.sprite(150, 400, 'direita', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.direita.setFrame(1)
+        this.personagem.anims.play('Derek-direita', true)
+        this.personagem.setVelocityX(100)
+      })
+      .on('pointerup', () => {
+        this.direita.setFrame(0)
+        this.personagem.anims.play('Derek-parado')
+        this.personagem.setVelocityX(0)
+      })
+
+    this.esquerda = this.add.sprite(50, 400, 'esquerda', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.esquerda.setFrame(1)
+        this.personagem.anims.play('Derek-esquerda', true)
+        this.personagem.setVelocityX(-100)
+      })
+      .on('pointerup', () => {
+        this.esquerda.setFrame(0)
+        this.personagem.anims.play('Derek-parado')
+        this.personagem.setVelocityX(0)
+      })
+
+    this.cima = this.add.sprite(700, 400, 'cima', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.cima.setFrame(1)
+        this.personagem.anims.play('Derek-cima', true)
+        this.personagem.setVelocityY(-300)
+      })
+      .on('pointerup', () => {
+        this.cima.setFrame(0)
+        this.personagem.anims.play('Derek-parado')
+        this.personagem.setVelocityY(0)
+      })
   }
 
   update () { }
